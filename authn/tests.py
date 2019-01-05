34a43b70fa911e3713da4761f35889a8118432cb
trejsu <trejsu@gmail.com>
2019-01-05 15:28:22 +0100

    Adds unit tests

--- authn/tests.py
+++ authn/tests.py
@@ -1,1 +1,34 @@
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+
+class LogoutViewTest(TestCase):
+
+    def setUp(self):
+        User.objects.create_user('test', 'test@test.com', 'pass123')
+
+    def test_logout_should_redirect_to_login_page(self):
+        # given:
+        username = 'test'
+        password = 'pass123'
+        self.client.login(username=username, password=password)
+
+        # when:
+        response = self.client.get(reverse('logout'))
+
+        # then:
+        self.assertRedirects(response, '/auth/login/')
+
+    def test_logout_should_work(self):
+        # given:
+        username = 'test'
+        password = 'pass123'
+        self.client.login(username=username, password=password)
+
+        # when:
+        self.client.get(reverse('logout'))
+        response = self.client.get(reverse('todo:home'))
+
+        # then:
+        self.assertRedirects(response, '/auth/login/?next=/todo/')
--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeViewTests(TestCase):
+
+    def setUp(self):
+        User.objects.create_user('test', 'test@test.com', 'pass123')
+
+    def test_unauthenticated_user_should_be_redirected_to_login_page(self):
+        # when:
+        response = self.client.get(reverse('todo:home'))
+
+        # then:
+        self.assertRedirects(response, '/auth/login/?next=/todo/')
+
+    def test_authenticated_user_should_stay_on_home_page(self):
+        # given:
+        username = 'test'
+        password = 'pass123'
+        self.client.login(username=username, password=password)
+
+        # when:
+        response = self.client.get(reverse('todo:home'))
+
+        # then:
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, f'Hi {username}!')
