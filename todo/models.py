cb7ce98499e2d6ed82eb6794c5025a56aae8b5d7
trejsu <trejsu@gmail.com>
2019-01-07 21:46:09 +0100

    Adds Task model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserData(models.Model):
+    user = models.ForeignKey(User, editable=False, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Task(UserData):
+    text = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text
