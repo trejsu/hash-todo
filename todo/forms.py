8d4e9498baf10dc5a3ea9ca3815e16eef3c4d246
trejsu <trejsu@gmail.com>
2019-01-27 16:14:37 +0100

    Improves add todo modal style

--- todo/forms.py
+++ todo/forms.py
@@ -7,3 +7,7 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['text']
+
+    def __init__(self, *args, **kwargs):
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        self.fields['text'].label = ''
